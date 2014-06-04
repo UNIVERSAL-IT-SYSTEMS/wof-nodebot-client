0167cc8cf1a33c0c6601fe40e23a6672b740cad9
Conner Brooks <t-cobroo@microsoft.com>
2014-06-04 15:31:56 -0700

    Initial commit of server

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser());
+app.use(bodyParser.json({type : 'application/vnd.api+json'}));
+
+var Robot = require('./app/models/robot');
+var robot = new Robot('4'); // give robot id
+
+app.post('/robot', function(req, res) {
+  console.log(req.body);
+  var direction = req.body.direction,
+      speed = req.body.speed,
+      duration = req.body.duration;
+  robot.move(direction, speed, duration);
+});
+var server = app.listen(3000, function() {
+  console.log('Listening on port %d', server.address().port);
+});

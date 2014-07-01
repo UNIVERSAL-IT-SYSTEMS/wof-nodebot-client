7a32b0f209aaba035516fda197be21490ff5b490
Conner Brooks <connerbrooks@gmail.com>
2014-07-01 10:46:02 -0700

    Formatting updates

--- server.js
+++ server.js
@@ -9,11 +9,17 @@ var Robot = require('./app/models/robot');
 var robot = new Robot('4'); // give robot id
 
 app.post('/robot', function(req, res) {
-  console.log(req.body);
-  var direction = req.body.direction,
-      speed = req.body.speed,
-      duration = req.body.duration;
-  robot.move(direction, speed, duration);
+	console.log(req.body);
+	var direction = req.body.direction,
+	speed = req.body.speed,
+	duration = req.body.duration;
+	robot.move(direction, speed, duration);
+});
+app.post('/robot/list', function(req, res) {
+	console.log(req.body);
+	var commandList = req.body;
+	for (var command in directionList) 
+		robot.move(command.direction, command.speed, command.duration);
 });
 var server = app.listen(3000, function() {
   console.log('Listening on port %d', server.address().port);
